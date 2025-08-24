842862bc6a411180ca60cbbf85314488981a6402
Asif Reman <asifremanbd@gmail.com>
2025-08-24 07:01:25 +0600

    Fix Vite config: Add CSS files to build process

--- energy-monitor/vite.config.js
+++ energy-monitor/vite.config.js
@@ -5,6 +5,9 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
+                'resources/css/app.css',
+                'resources/css/dashboard-customization.css',
+                'resources/css/rtu-widgets.css',
                 'resources/js/app.js',
                 'resources/js/rtu-sections.js',
                 'resources/js/rtu-widgets.js'
